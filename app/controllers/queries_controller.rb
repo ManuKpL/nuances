fd3f90f74cbf244c05cfdde24dcf9aca49ea8d5a
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-05-29 18:41:52 +0200

    added all remaining actions to query controller

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,6 +1,22 @@
 class QueriesController < ApplicationController
 
   def index
+    @queries = Query.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
-  resources :queries, only: :index
+  resources :queries
   root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
