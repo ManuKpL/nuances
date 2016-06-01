9c2d76025accceb81b1ff99eaf1e869db34c13ec
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-06-01 19:11:35 +0200

    added dependence to models

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,7 +1,7 @@
 class Query < ActiveRecord::Base
   belongs_to :theme
-  has_many :answers
-  has_many :choices
+  has_many :answers, dependent: :restrict_with_error
+  has_many :choices, dependent: :destroy
 
   def name
     content
--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,5 +1,5 @@
 class Theme < ActiveRecord::Base
-  has_many :queries
+  has_many :queries, dependent: :restrict_with_error
   has_many :choices, through: :queries
   has_many :answers, through: :queries
 end
