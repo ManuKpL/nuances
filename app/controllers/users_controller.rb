2de7b0894240b365b13f08203784c5509752955c
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-06-09 01:46:27 +0200

    moved user-card to partial + more implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: :show
+  before_action :set_themes, only: [:show, :index]
+  before_action :set_bgs, only: [:show, :index]
+
   def show
   end
 
@@ -19,5 +23,25 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:nickname, :first_name, :last_name, :email, :twitter, :facebook, :photo, :photo_cache)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def set_bgs
+    @backgrounds = %w(bg-dark-blue bg-blue bg-light-blue)
+  end
+
+  def set_themes
+    @themes = Array.new
+    Theme.order(:name).each do |theme|
+      count = 0
+      theme.queries.each do |query|
+        count +=1 if query.users.include?(@user)
+      end
+      percentage = (count * 100.0 / theme.queries.count).round(0).to_s << "%"
+      @themes << { name: theme.name, queries: percentage }
+    end
   end
 end
