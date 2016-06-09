d7259748efc189e5f2e0711ba052952c12d236e6
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-06-09 14:37:00 +0200

    fixed bug when no answers yet by user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,6 +7,8 @@ module UsersHelper
       global = versus.answers.map(&:choice_id)
       user_answers.each { |answer| count += global.count(answer) - 1 }
       return (count * 100.0 / (global.count - user_answers.count)).round.to_s << '%'
+    elsif user_answers.empty?
+      return 'N/R'
     else
       global = versus.answers.where(user_id: user).map(&:choice_id)
       user_answers.each { |answer| count += global.count(answer) }
