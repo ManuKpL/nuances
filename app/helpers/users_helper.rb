31d029ba3f63002c0c0fe5a8dd78ea5e7f468a67
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-06-09 23:47:10 +0200

    added percentage of responses

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,15 +4,30 @@ module UsersHelper
     count = 0
     user_answers = theme.answers.where(user_id: user).map(&:choice_id)
     if user_answers.empty?
-      return 'N/R'
+      percentage = 'N/R'
+      user_data = {
+        theme_percentage: percentage,
+        total_percentage: (user.answers.count * 100.0 / Query.all.count).round.to_s << '%'
+      }
+      return user_data
     elsif scope == 'all'
       global = theme.answers.map(&:choice_id)
       user_answers.each { |answer| count += global.count(answer) - 1 }
-      return (count * 100.0 / (global.count - user_answers.count)).round.to_s << '%'
+      percentage = (count * 100.0 / (global.count - user_answers.count)).round.to_s << '%'
+      user_data = {
+        theme_percentage: percentage,
+        total_percentage: (user.answers.count * 100.0 / Query.all.count).round.to_s << '%'
+      }
+      return user_data
     else
       global = theme.answers.where(user_id: scope).map(&:choice_id)
       user_answers.each { |answer| count += global.count(answer) }
-      return  (count * 100.0 / user_answers.count).round.to_s << '%'
+      percentage = (count * 100.0 / user_answers.count).round.to_s << '%'
+      user_data = {
+        theme_percentage: percentage,
+        total_percentage: (user.answers.count * 100.0 / Query.all.count).round.to_s << '%'
+      }
+      return user_data
     end
   end
 
